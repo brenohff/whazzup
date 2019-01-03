93149659906f33d0d0943498f0b3ae70f5a6f812
breno.franco <breno.franco@cabal.com.br>
2019-01-03 14:00:08 -0200

    First commit

---
+++ src/main/java/com/brenohff/whazzup/whazzup/WhazzupApplication.java
@@ -0,0 +1,14 @@
+package com.brenohff.whazzup.whazzup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhazzupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhazzupApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/com/brenohff/whazzup/whazzup/WhazzupApplicationTests.java
@@ -0,0 +1,17 @@
+package com.brenohff.whazzup.whazzup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WhazzupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
